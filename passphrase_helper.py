7b4cfdd60f128c657ea3c5e52aeb005ba00b04bf
tofran <franciscopcmarques@gmail.com>
2018-04-16 23:27:01 +0100

    More configurable

--- passphrase_helper.py
+++ passphrase_helper.py
@@ -1,6 +1,10 @@
 
 
 ALWAYS_STARTS_WITH_KNOWN_LETTER = True
+MAX_LENGHT_VARIATION = 1
+FOUND_CHARS_LENGHT_MULTI = 0.9
+
+WORD_SEPARATOR = ' '
 
 dictionary = []
 
@@ -10,16 +14,17 @@ with open('bip_039_wordlist.txt', 'r') as dictionary_file:
 
 print("Loaded {} words".format(len(dictionary)))
 
-known_passphrase = 'XXXXXXXXXXXX'
+known_passphrase = None
+if not known_passphrase:
+    known_passphrase = input('Input the known passphrase: ')
 
-known_words = known_passphrase.split(' ')
+known_words = known_passphrase.split(WORD_SEPARATOR)
 
 print("Looking for passphrases similar to: {}\n".format(known_words))
 
 for word in known_words:
     word_lengh = len(word)
-    score_needed = int(0.9*word_lengh)
-    # lenght_variation = int(0.1*word_lengh)
+    score_needed = int(FOUND_CHARS_LENGHT_MULTI*word_lengh)
     possible_matches = []
     for dictionary_word in dictionary:
         score = 0
@@ -27,7 +32,7 @@ for word in known_words:
             ALWAYS_STARTS_WITH_KNOWN_LETTER and
             dictionary_word.startswith(word[0]) and
             dictionary_word != word and
-            abs(word_lengh-len(dictionary_word)) < 2
+            abs(word_lengh-len(dictionary_word)) <= MAX_LENGHT_VARIATION
         ):
             for char in word:
                 if char in dictionary_word:
