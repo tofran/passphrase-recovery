e73c113b9a159069bf8788c764213412493aa13e
tofran <franciscopcmarques@gmail.com>
2020-11-08 23:45:16 +0000

    Improved project setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended"
+    "plugin:react-hooks/recommended" // TODO: understad why I need this (eslint-plugin-react-hooks)
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
